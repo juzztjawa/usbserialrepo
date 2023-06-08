e8b02c80e292bc364014c444916a1bce2547d272
juzztjawa <112401440+juzztjawa@users.noreply.github.com>
2023-06-08 13:36:30 +0530

    usbserial
    new addition

---
+++ test/widget_test.dart
@@ -0,0 +1,16 @@
+void main() {}
+// void setup() {
+//   // put your setup code here, to run once:
+//   Serial.begin(115200);
+//   Serial.println("Hello, ESP32!");
+//   Serial.flush();
+// }
+
+// void loop() {
+//   while(Serial.available()>0){
+//   uint8_t bytefromSerial=Serial.read();
+//   uint8_t buff[100]= {bytefromSerial};
+//   String str=(char*)buff;
+//   Serial.print(str);
+// }
+// }
